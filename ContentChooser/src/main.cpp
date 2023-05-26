de054a9ac6111ec76a3250152c841cdefea3d040
kiwiivanya128@gmail.com <kiwiivanya128@gmail.com>
2023-05-26 23:30:27 +0300

    made a concept and made set & get commands

--- ContentChooser/src/main.cpp
+++ ContentChooser/src/main.cpp
@@ -6,125 +6,9 @@
 
 #include "MySQLBank.h"
 
-#include <vector>
-/*
 int main()
 {
-	//for demonstration only. never save your password in the code!
-	std::string server;
-	std::string username;
-	std::string password;
-
-	std::ifstream fin("../resource/dbticket.txt");
-	if (!fin.is_open())
-	{
-		std::cout << "\"dbticket.txt\" isn't exist\n";
-		system("pause");
-		exit(1);
-	}
-
-	std::getline(fin, server);
-	std::getline(fin, username);
-	std::getline(fin, password);
-
-	fin.close();
-
-	sql::Driver* driver; // sql drivers for further connection
-	sql::Connection* con; // connection statement itself
-	sql::Statement* stmt; // instance statement of db (execute management of databases or table configurations)
-	sql::PreparedStatement* pstmt; // prepared statement of db (execute queries)
-	sql::ResultSet* res; // result of query
-
-	try
-	{
-		driver = get_driver_instance();
-		con = driver->connect(server, username, password);
-	}
-	catch (sql::SQLException e)
-	{
-		std::cout << "Could not connect to server. Error message: " << e.what() << '\n';
-		system("pause");
-		exit(1);
-	}
-
-	//please create database "quickstartdb" ahead of time
-	stmt = con->createStatement();
-	stmt->execute("CREATE DATABASE quickstartdb");
-
-	con->setSchema("quickstartdb"); // select schema (database)
-
-	
-	stmt->execute("DROP TABLE IF EXISTS inventory");
-	std::cout << "Finished dropping table (if existed)" << '\n';
-	stmt->execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);");
-	std::cout << "Finished creating table" << '\n';
-
-
-	pstmt = con->prepareStatement("INSERT INTO inventory(name, quantity) VALUES(?,?)"); // questions marks is filling not imidiatly (like templates for further queries)
-	pstmt->setString(1, "banana");
-	pstmt->setInt(2, 150);
-	pstmt->execute(); // execution
-	std::cout << "One row inserted." << '\n';
-
-	pstmt->setString(1, "orange");
-	pstmt->setInt(2, 154);
-	pstmt->execute();
-	std::cout << "One row inserted." << '\n';
-
-	pstmt->setString(1, "apple");
-	pstmt->setInt(2, 100);
-	pstmt->execute();
-	std::cout << "One row inserted." << '\n';
-
-	stmt->execute("DROP DATABASE quickstartdb");
-	std::cout << "Deleted unnecessary db\n";
-	
-	// go to world db for testing
-	con->setSchema("sakila");
-	std::cout << "Select db \"sakila\"\n";
-
-	// get cities
-	stmt = con->createStatement();
-	res = stmt->executeQuery("SELECT first_name, last_name FROM actor ORDER BY last_name ASC");
-	//res = stmt->executeQuery("SELECT * FROM city WHERE CountryCode LIKE \"UKR\" AND Population > 500000 ORDER BY Population DESC");
-	std::cout << "Got data\n";
-	
-	//get result set metadata
-	sql::ResultSetMetaData* res_meta = res->getMetaData();
-	int columns = res_meta->getColumnCount();
-	std::cout << "Got metadata\n";
-	std::cout << "Output\n";
-
-	std::cout << '\n';
-
-	//Loop for each row
-	while (res->next()) {
-		/* Access column data by index, 1-indexed
-		for (int i = 1; i <= columns; i++) {
-			std::cout << res->getString(i) << "\t|";
-		}
-		std::cout << '\n';
-
-		std::cout << res->getString(2) << " ";
-		std::cout << res->getString(1) << "\n";
-		//std::cout << res->getString(5) << '\n';
-	}
-
-	std::cout << '\n';
-
-	con->close();
-
-	delete pstmt;
-	delete con;
-	delete stmt;
-	delete res;
-	system("pause");
-	return 0;
-}
-*/
-int main()
-{
-	MySQLBank<true> bank;
+	MySQLBank<false> bank;
 	if (bank.connect("../resource/dbticket.txt") != 0)
 	{
 		printf("No connection or no key");
@@ -133,15 +17,36 @@ int main()
 	bank.immediatelyExecute("DROP DATABASE IF EXISTS content_chooser;");
 	bank.immediatelyExecute("CREATE DATABASE content_chooser;");
 	bank.selectScheme("content_chooser");
+	printf("deleted and created database\n");
 
-	bank.immediatelyExecute("CREATE TABLE name(id serial PRIMARY KEY, title varchar(128));");
+	bank.immediatelyExecute("CREATE TABLE content_name(id serial PRIMARY KEY, title varchar(128), duration_m smallint unsigned);");
+	printf("created table\n");
 
-	bank.prepareExecution("INSERT INTO name(title) VALUES(?)");
+	bank.prepareExecution("INSERT INTO content_name(title, duration_m) VALUES(?, ?)");
 	bank.getPreparedStatementInstance()->setString(1,"Star versus the Forces of Evil");
+	bank.getPreparedStatementInstance()->setInt(2, 22);
 	bank.executePrepared();
+	printf("executed\n");
 
 	bank.getPreparedStatementInstance()->setString(1, "Amphibia");
+	bank.getPreparedStatementInstance()->setInt(2, 23);
 	bank.executePrepared();
+	printf("executed\n");
+
+	bank.executeQuery("SELECT * FROM content_name");
+	printf("got\n");
+
+	size_t col = bank.getResultColomns();
+	auto res = bank.getResultInstance();
+	printf("prepared\n");
+
+	while (res->next())
+	{
+		for (size_t i = 1; i <= col; i++)
+			std::cout << res->getString(i) << '\t';
+
+		std::cout << "\n";
+	}
 
 	return 0;
 }
